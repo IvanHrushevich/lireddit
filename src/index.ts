f66fb2a12a3a4629288deb9e7a8e54d5b95377fe
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2022-10-24 04:36:07 +0300

    implement initial MikroOrm setup, connect to db

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('Hello world');
+import { MikroORM } from '@mikro-orm/core';
+
+import { __prod__ } from './constants';
+import { Post } from './entities/Post';
+import mikroOrmConfig from './mikro-orm.config';
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroOrmConfig);
+  await orm.getMigrator().up();
+
+  const fork = orm.em.fork();
+
+  // TODO: fix bug with id
+  const post = fork.create(Post, { title: 'My first Post', id: 3 });
+  await fork.persistAndFlush(post);
+};
+
+main().catch((error) => console.log('[error in main]', error));
