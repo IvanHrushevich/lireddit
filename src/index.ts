53d6ddfef8f0209eaa339888a87242f8a4477ca3
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2022-10-24 12:40:03 +0300

    set up GraphQl server with dummy resolver

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { MikroORM } from '@mikro-orm/core';
 import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
 
 import { __prod__ } from './constants';
 import mikroOrmConfig from './mikro-orm.config';
+import { HelloResolver } from './resolvers/hello';
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
@@ -13,6 +16,16 @@ const main = async () => {
   await generator.updateSchema();
 
   const app = express();
+
+  // GraphQL server
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
 
   app.listen(4000, () => {
     console.log('server is started on port 4000');
