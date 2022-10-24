99ffdb11a3bd21ba003b13fd5b7075dead6771c2
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2022-10-25 01:03:49 +0300

    add some Queries and Mutations to PostResolver

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,12 +1,58 @@
-import { Resolver, Query, Ctx } from 'type-graphql';
+import { Resolver, Query, Ctx, Arg, Mutation } from 'type-graphql';
 
 import { MyContext } from './../types';
 import { Post } from '../entities/Post';
+import { v4 as uuidv4 } from 'uuid';
 
 @Resolver()
 export class PostResolver {
   @Query(() => [Post])
   posts(@Ctx() ctx: MyContext): Promise<Post[]> {
     return ctx.emFork.find(Post, {});
+  }
+
+  @Query(() => Post, { nullable: true })
+  post(
+    @Arg('id')
+    id: string,
+    @Ctx()
+    ctx: MyContext
+  ): Promise<Post | null> {
+    return ctx.emFork.findOne(Post, { id });
+  }
+
+  @Mutation(() => Post)
+  async createPost(
+    @Arg('title')
+    title: string,
+    @Ctx()
+    ctx: MyContext
+  ): Promise<Post> {
+    const post = await ctx.emFork.create(Post, { title, id: uuidv4() });
+    await ctx.emFork.persistAndFlush(post);
+    return post;
+  }
+
+  @Mutation(() => Post, { nullable: true })
+  async updatePost(
+    @Arg('id')
+    id: string,
+    @Arg('title', { nullable: true })
+    title: string,
+    @Ctx()
+    ctx: MyContext
+  ): Promise<Post | null> {
+    const post = await ctx.emFork.findOne(Post, { id });
+
+    if (!post) {
+      return null;
+    }
+
+    if (typeof title !== 'undefined') {
+      post.title = title;
+      await ctx.emFork.persistAndFlush(post);
+    }
+
+    return post;
   }
 }
