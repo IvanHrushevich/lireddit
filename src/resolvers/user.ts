568cf16239c4ce9b7f029212e11635e76802bc14
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2022-10-25 16:11:29 +0300

    add mutation for login

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,4 +1,4 @@
-import { Arg, Ctx, Field, InputType, Mutation, Resolver } from 'type-graphql';
+import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Resolver } from 'type-graphql';
 import { v4 as uuidv4 } from 'uuid';
 import argon2 from 'argon2';
 
@@ -14,6 +14,24 @@ class UsernamePasswordInput {
   password: string;
 }
 
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
+
 @Resolver()
 export class UserResolver {
   @Mutation(() => User)
@@ -22,5 +40,20 @@ export class UserResolver {
     const user = emFork.create(User, { username: options.username, id: uuidv4(), password: hashedPassword });
     await emFork.persistAndFlush(user);
     return user;
+  }
+
+  @Mutation(() => UserResponse)
+  async login(@Arg('options') options: UsernamePasswordInput, @Ctx() { emFork }: MyContext): Promise<UserResponse> {
+    const user = await emFork.findOne(User, { username: options.username });
+    if (!user) {
+      return { errors: [{ field: 'username', message: "username doesn't exist" }] };
+    }
+
+    const isPasswordValid: boolean = await argon2.verify(user.password, options.password);
+    if (!isPasswordValid) {
+      return { errors: [{ field: 'password', message: 'incorrect password' }] };
+    }
+
+    return { user };
   }
 }
