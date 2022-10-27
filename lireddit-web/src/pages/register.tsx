8404acb3c4b5a9f32ffacb8329c8809ad4789986
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2022-10-27 14:17:14 +0300

    FE: perform refactoring of register, update register submit

--- lireddit-web/src/pages/register.tsx
+++ lireddit-web/src/pages/register.tsx
@@ -1,36 +1,31 @@
 import { Box, Button } from '@chakra-ui/react';
 import { Form, Formik } from 'formik';
 import { FC } from 'react';
-import { useMutation } from 'urql';
+import { useRouter } from 'next/router';
 
 import InputField from '../components/InputField';
 import Wrapper from '../components/Wrapper';
-
-const REGISTER_MUT: string = `
-mutation Register($username: String!, $password: String!) {
-    register(options: { username: $username, password: $password }) {
-      errors {
-        field
-        message
-      }
-      user {
-        username
-        id
-      }
-    }
-  }
-`;
+import { useRegisterMutation } from '../generated/graphql';
+import { toErrorMap } from '../util/toErrorMap';
 
 const register: FC = () => {
-  const [{}, register] = useMutation(REGISTER_MUT);
+  const [{}, register] = useRegisterMutation();
+  const router = useRouter();
 
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: '', password: '' }}
-        onSubmit={async (values) => {
+        onSubmit={async (values, { setErrors }) => {
           const response = await register(values);
-          console.log('response', response);
+          const errors = response.data?.register.errors;
+          const user = response.data?.register.user;
+
+          if (errors) {
+            setErrors(toErrorMap(errors));
+          } else if (user) {
+            router.push('/');
+          }
         }}
       >
         {({ isSubmitting }) => (
